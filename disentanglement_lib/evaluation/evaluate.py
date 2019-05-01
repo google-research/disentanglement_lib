b41706ea4785e770084001e4fa387daaa7d12ddd
gabb7 <gabb@robots.ox.ac.uk>
2019-05-01 15:09:19 +0100

    refactoring

--- disentanglement_lib/evaluation/evaluate.py
+++ disentanglement_lib/evaluation/evaluate.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Evaluation protocol to compute metrics."""
+"""Evaluation protocol o compute metrics."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
